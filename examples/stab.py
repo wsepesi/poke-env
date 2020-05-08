59b427cfaa316104c359fd1c7326429642015720
William Sepesi <w.sepesi@att.net>
2020-05-08 12:29:04 -0500

    Create stab.py

---
+++ examples/stab.py
@@ -0,0 +1,39 @@
+    def embed_battle(self, battle):
+        # -1 indicates that the move does not have a base power
+        # or is not available
+        moves_base_power = -np.ones(4)
+        moves_dmg_multiplier = np.ones(4)
+
+        def stab(move):
+            if (battle.active_pokemon).type_1 == move.type or (battle.active_pokemon).type_2 == move.type:
+                return 1.5
+
+            else:
+                return 1
+
+        for i, move in enumerate(battle.available_moves):
+            moves_base_power[i] = (
+                move.base_power / 100
+            )  # Simple rescaling to facilitate learning
+            if move.type:
+                moves_dmg_multiplier[i] = move.type.damage_multiplier(
+                    battle.opponent_active_pokemon.type_1,
+                    battle.opponent_active_pokemon.type_2,
+                ) * stab(move)
+
+        # We count how many pokemons have not fainted in each team
+        remaining_mon_team = (
+            len([mon for mon in battle.team.values() if mon.fainted]) / 6
+        )
+        remaining_mon_opponent = (
+            len([mon for mon in battle.opponent_team.values() if mon.fainted]) / 6
+        )
+
+        # Final vector with 10 components
+        return np.concatenate(
+            [
+                moves_base_power,
+                moves_dmg_multiplier,
+                [remaining_mon_team, remaining_mon_opponent],
+            ]
+        )
